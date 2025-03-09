3e90a00a177c9d815ad796315cf5d1eefe2c5717
Underdisc <connortdeakin@gmail.com>
2025-03-09 16:50:24 +0100

    AutoHotkey breakout path adjustment

--- start.bat
+++ start.bat
@@ -5,19 +5,10 @@ set octane=OCTANE
 
 set ahk_script=dotfiles\.ahk
 if %hostname% == %octane% (
-  set ahk_program=%userprofile%\home\sys\win64\prgf\auto_hotkey_2.0\v2\AutoHotkey64.exe
-  set wincompose_program=%userprofile%\home\sys\win64\prgf\WinCompose_0.9.11\wincompose.exe
-  goto start_ahk
+  start %userprofile%\home\sys\win64\prgf\auto_hotkey_2.0\v2\AutoHotkey64.exe %ahk_script%
+  start %userprofile%\home\sys\win64\prgf\WinCompose_0.9.11\wincompose.exe
 )
 if %hostname% == %breakout% (
-  set ahk_program=C:\home\sys\prgf\auto_hotkey\AutoHotKey64.exe
-  goto start_ahk
+  start C:\home\sys\win64\prgf\AutoHotkey_2.0.19\AutoHotkey64.exe %ahk_script%
 )
-goto end
 
-:start_ahk
-start %ahk_program% %ahk_script%
-start %wincompose_program%
-goto end
-
-:end
