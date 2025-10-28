844469038e374aebf4baeee341740ac8edc36375
Underdisc <undydisc@gmail.com>
2025-10-28 11:56:59 +0100

    Setup symlinks in directories and git number
    Before this, setup only created symlinks in ~/ for the files contained in home/ (this repo's home/ directory). In addition to this, it now creates directories contained in home/ that are missing in ~/ and creates symlinks for the files contained within those directories.
    
    It also creates the necessary symlinks needed to use the git-number scripts. This is supplemented by the additional directory added to PATH.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
-# Usage: ./setup.sh <fullpath_to_home>
-# fullpath_to_home is the full path to your home directory (~/)
-# For example: C:/cygwin64/home/underdisc
+# Usage: ./setup.sh <fullpath_to_home>?
+# fullpath_to_home: The full path to your home directory. If not included, ~ is
+#   used as the default.
 
-home_full_path=$1
-script_full_path=$(realpath "$0")
-script_dir=$(dirname "$script_full_path")
+home_path=$(realpath ~)
+if [ ! -z $1 ]; then
+  home_path=$1
+fi
+script_path=$(realpath "$0")
+script_dir=$(dirname "$script_path")
 
-# This will allow us to account for dotfiles. Dotfiles are not
-# extracted from a directory in a shell script by default.
+ensure_dirs() {
+  local dirs=$1
+  for dir in $dirs; do
+    if [ ! -d "$dir" ]; then
+      mkdir "$dir"
+    fi
+  done
+}
+
+symlink_home_files() {
+  for entry_path in $(find $script_dir/home -printf '%P\n' | sort); do
+    local repo_entry_path=$script_dir/home/$entry_path
+    local home_entry_path=$home_path/$entry_path
+
+    # Create a directory at home if it doesn't exist.
+    if [ -d "$repo_entry_path" ]; then
+      ensure_dirs "$home_entry_path"
+      continue
+    fi
+
+    # Create a symlink for the entry.
+    if [ -f "$home_entry_path" ] || [ -L "$home_entry_path" ]; then
+      rm "$home_entry_path"
+    fi
+    ln -s "$repo_entry_path" "$home_entry_path"
+  done
+}
+
+symlink_git_number_files() {
+  local git_number_files="git-id git-list git-number"
+  for git_number_entry in $git_number_files; do
+    local repo_entry_path="$script_dir/sub/git-number/$git_number_entry"
+    local bin_entry_path="$home_path/.local/bin/$git_number_entry"
+    if [ -f "$bin_entry_path" ] || [ -L "$bin_entry_path" ]; then
+      rm "$bin_entry_path"
+    fi
+    ln -s "$repo_entry_path" "$bin_entry_path"
+  done
+}
+
+# shopt will allow us to account for dotfiles. Dotfiles are not extracted from a
+# directory in a shell script by default.
 shopt -s dotglob
-
-# Create a symlink for every entry in home/. Existing files are overwritten.
-for repo_dotfile in $script_dir/home/*; do
-  home_dotfile=$home_full_path/$(basename $repo_dotfile)
-  if [ -f "$home_dotfile" ] || [ -L "$home_dotfile" ]; then
-    rm $home_dotfile
-  fi
-  ln -s $repo_dotfile $home_dotfile
-done
-
+symlink_home_files
 shopt -u dotglob
+
+ensure_dirs "$home_path/.local $home_path/.local/bin"
+symlink_git_number_files
