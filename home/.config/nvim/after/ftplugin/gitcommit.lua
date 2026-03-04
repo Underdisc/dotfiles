01159a3baafc4252d561e76af946a0de079801ba
Underdisc <undydisc@gmail.com>
2026-03-04 15:46:35 +0100

    Neovim Git Commit Settings
    Disable autowrapping, enable spelling, and disable the color column.

---
+++ home/.config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,7 @@
+-- Disable automatic line wrapping.
+vim.opt_local.formatoptions:remove('t')
+vim.opt_local.formatoptions:remove('l')
+
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en"
+vim.opt_local.colorcolumn = ""
