fb71d2e77cb05ec9d42aa881a4bf581e4026073c
Underdisc <undydisc@gmail.com>
2026-06-06 15:39:31 +0200

    Quick Git Log
    Pressing ' gl' will show the git log for the current focused line in a popup window. Viewing a line's commit log can be achieved by using vim fugitive's blame feature, but I found that to be too many extra steps.
    
    I debated on ensuring that the focused line is always visible, but I'm doubtful that it would bring an tangible benefit.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -285,6 +285,76 @@ vim.api.nvim_set_hl(0, 'GitSignsStagedDeleteNr', { fg = '#905555' })
 vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { fg = '#55ccee' })
 vim.api.nvim_set_hl(0, 'GitSignsStagedChangeNr', { fg = '#558899' })
 
+-- Set git commit message view colors.
+local git_log_ns = vim.api.nvim_create_namespace('GitLog')
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'GitSha', { fg = '#00dddd' })
+vim.api.nvim_set_hl(0, 'GitDate', { fg = '#ee44ff' })
+vim.api.nvim_set_hl(0, 'GitAuthor', { fg = '#44ee44' })
+vim.api.nvim_set_hl(0, 'GitSummary', { link = 'Title' })
+
+-- Quickly view the commit message for a line.
+vim.keymap.set('n', '<leader>gl', function()
+  -- Get the commit sha of the current line.
+  local file = vim.fn.expand('%:p')
+  local directory = vim.fn.expand('%:p:h')
+  local line_number = vim.fn.line('.')
+  local line_range = line_number .. ',' .. line_number
+  local blame_cmd = { 'git', 'blame', '-L', line_range, '--porcelain', file }
+  local result = vim.system(blame_cmd, { cwd = directory, text = true }):wait()
+  local sha = vim.split(result.stdout, ' ')[1]
+
+  -- Put the commit's log in a new buffer, but return if there is no log.
+  local date_format = '--date=format:%y-%m-%d'
+  local log_format = '--format=%h %ad %an%n%B'
+  local log_cmd = { 'git', 'log', '-n 1', date_format, log_format, sha }
+  result = vim.system(log_cmd, { cwd = directory, text = true }):wait()
+  if result.stdout == '' then return end
+  local buff_lines = vim.split(result.stdout, '\n')
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, buff_lines)
+  vim.bo[bufnr].modifiable = false
+
+  -- Apply highlights to information shared amongst all logs.
+  local sha_start = { 0, 0 }
+  local sha_end = { 0, string.find(buff_lines[1], ' ') - 1 }
+  local date_start = { 0, sha_end[2] + 1 }
+  local date_end = { 0, string.find(buff_lines[1], ' ', date_start[2] + 1) - 1 }
+  local author_start = { 0, date_end[2] + 1 }
+  local author_end = { 0, #buff_lines[1] }
+  local summary_start = { 1, 0 }
+  local summary_end = { 1, #buff_lines[2] }
+  vim.hl.range(bufnr, git_log_ns, 'GitSha', sha_start, sha_end)
+  vim.hl.range(bufnr, git_log_ns, 'GitDate', date_start, date_end)
+  vim.hl.range(bufnr, git_log_ns, 'GitAuthor', author_start, author_end)
+  vim.hl.range(bufnr, git_log_ns, 'GitSummary', summary_start, summary_end)
+
+  -- Open a popup window with the log buffer at the top of the current window.
+  local current_window_width = vim.api.nvim_win_get_width(0)
+  local padding = 5
+  local max_width = 80
+  local log_width = math.min(current_window_width - padding * 2, max_width)
+  local winid = vim.api.nvim_open_win(bufnr, true, {
+    border = 'single',
+    relative = 'win',
+    width = log_width,
+    height = 10,
+    row = 0,
+    col = padding - 1,
+  })
+  vim.wo[winid].number = false
+  vim.wo[winid].relativenumber = false
+
+  -- Close the log window and delete the buffer if it loses focus.
+  vim.api.nvim_create_autocmd('WinLeave', {
+    once = true,
+    callback = function()
+      vim.api.nvim_win_close(winid, true)
+      vim.api.nvim_buf_delete(bufnr, { force = true })
+    end,
+  })
+end)
+
 -- Configure the command line.
 vim.opt.cmdheight = 1
 vim.opt.ruler = false
