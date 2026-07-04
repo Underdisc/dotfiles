6ab1af33265949bbea745f83a87f03c53e7c8e68
Underdisc <undydisc@gmail.com>
2026-07-05 00:01:50 +0200

    polybar: breakout
    Polybar didn't work on breakout because of an incorrect monitor name. Breakout also requires unique polybar widgets which will come in later commits. A wifi and battery widget are what I currently have in mind. All of this requires a new polybar for breakout. Due to how the polybar is instantiated, a script is needed instantiate different polybars depending on the host. This script is ran when i3 is launched.

---
+++ script/polybar-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+hostname=$(hostname)
+if [[ $hostname =~ breakout ]]; then
+  polybar breakout_primary
+elif [[ $hostname =~ octane ]]; then
+  polybar octane_primary
+  polybar octane_secondary
+fi
