dc424b3cb94e86b1851a920107cc3f7a5615fef8
Underdisc <undydisc@gmail.com>
2026-07-10 04:44:29 +0200

    polybar: Setup Detach
    The polybar on octane's second monitor didn't appear because `polybar octane_primary` blocked further execution.

--- script/polybar-setup.sh
+++ script/polybar-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 hostname=$(hostname)
 if [[ $hostname =~ breakout ]]; then
-  polybar breakout_primary
+  polybar breakout_primary &
 elif [[ $hostname =~ octane ]]; then
-  polybar octane_primary
-  polybar octane_secondary
+  polybar octane_primary &
+  polybar octane_secondary &
 fi
