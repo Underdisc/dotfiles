9ecaa7d07df75b8af772e72ea9b1441d367bd92d
Underdisc <undydisc@gmail.com>
2026-07-20 12:56:44 +0200

    yazi: Bring Prompt to Dir
    After navigating to a directory within yazi and exiting it with `q`, the working directory of the zsh session will change to the directory that was navigated to within yazi. This was found here `https://yazi-rs.github.io/docs/quick-start`

--- home/.config/shrc/all.sh
+++ home/.config/shrc/all.sh
@@ -26,12 +26,19 @@ alias rsyncd='rsync --daemon --no-detach'
 base_rsync_flags='--human-readable --recursive --times --modify-window=3 --devices --links --specials --verbose --itemize-changes --progress'
 alias archive="rsync $base_rsync_flags"
 alias reflect="rsync $base_rsync_flags --delete"
-alias yz='yazi'
 
 alias ls='ls --color=auto'
 alias la='ls -a'
 alias ll='ls -l --time-style=long-iso'
 alias lla='ls -la'
+
+yz() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  command rm -f -- "$tmp"
+}
 
 btop() {
   hostname=$(hostname)
